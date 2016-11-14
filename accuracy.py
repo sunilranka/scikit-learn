6b2884efed0d5d3ef0ba906a1eb34b828ddc738e
Sunil Ranka <sunil.ranka@jgus-2k16-MC4.local>
2016-11-14 00:20:46 -0800

    Initial Version

---
+++ accuracy.py
@@ -0,0 +1,76 @@
+#
+# In this and the following exercises, you'll be adding train test splits to the data
+# to see how it changes the performance of each classifier
+#
+# The code provided will load the Titanic dataset like you did in project 0, then train
+# a decision tree (the method you used in your project) and a Bayesian classifier (as
+# discussed in the introduction videos). You don't need to worry about how these work for
+# now. 
+#
+# What you do need to do is import a train/test split, train the classifiers on the
+# training data, and store the resulting accuracy scores in the dictionary provided.
+
+import numpy as np
+import pandas as pd
+
+# Load the dataset
+X = pd.read_csv('titanic_data.csv')
+# Limit to numeric data
+X = X._get_numeric_data()
+# Separate the labels
+y = X['Survived']
+# Remove labels from the inputs, and age due to missing data
+del X['Age'], X['Survived']
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
+
+# TODO: split the data into training and testing sets,
+# using the standard settings for train_test_split.
+# Then, train and test the classifiers with your newly split data instead of X and y.
+
+### Added by sranka - Sunil S Ranka 
+
+from sklearn import cross_validation
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y, test_size=0.4, random_state=0)
+
+print X_train.head()
+print "-------"
+print X_test.head()
+print "-------"
+print y_train.head()
+print "-------"
+print y_test.head()
+
+# The decision tree classifier
+clf1 = DecisionTreeClassifier()
+clf1.fit(X,y)
+print "Decision Tree has accuracy: ",accuracy_score(y, clf1.predict(X))
+
+# The decision tree classifier
+clf1 = DecisionTreeClassifier()
+clf1.fit(X_train,y_train)
+print "-------"
+print "-------"
+print "New Decision Tree has accuracy: ",accuracy_score(y_test, clf1.predict(X_test))
+print "-------"
+print "-------"
+
+
+# The naive Bayes classifier
+
+clf2 = GaussianNB()
+clf2.fit(X,y)
+print "GaussianNB has accuracy: ",accuracy_score(y, clf2.predict(X))
+
+clf2 = GaussianNB()
+clf2.fit(X_train,y_train)
+print "New -- GaussianNB has accuracy: ",accuracy_score(y_test, clf2.predict(X_test))
+
+
+answer = { 
+ "Naive Bayes Score": accuracy_score(y, clf2.predict(X)), 
+ "Decision Tree Score": accuracy_score(y_test, clf2.predict(X_test))
+}
+
